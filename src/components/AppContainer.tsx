6d55abf6d7ec4bc8926bc33c7b73d5a468d5b504
John_Lamy <johnclamy@gmail.com>
2023-02-10 15:51:25 +0000

    setup the AppContainer component

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+type AppContainerProps = {
+  children: ReactNode;
+};
+
+const AppContainer = ({ children }: AppContainerProps) => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} bg-white>
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AppContainer;
