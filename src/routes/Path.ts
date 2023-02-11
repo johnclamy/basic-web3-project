92a486749ef65dc37c75062016d2f390cdd802dc
John_Lamy <johnclamy@gmail.com>
2023-02-11 00:44:46 +0000

    create a constant for the route paths

---
+++ src/routes/Path.ts
@@ -0,0 +1,12 @@
+enum Path {
+  HOME = "/pets",
+  LANDING = "/",
+  SIGN_UP = "/signup",
+  SIGN_IN = "/signin",
+  ADD_PET = "/add-pet",
+  NOT_FOUND = "*",
+  PET_DETAILS = "/pets/:id",
+  PASSWORD_FORGET = "/password-forget",
+}
+
+export default Path;
