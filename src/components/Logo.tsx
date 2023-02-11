3773414fd8b410b1e70411b2d2e203087c90ce96
John_Lamy <johnclamy@gmail.com>
2023-02-11 00:42:33 +0000

    create the app logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import Badge from "react-bootstrap/Badge";
+
+const Logo = () => (
+  <>
+    <Badge bg="warning" text="white">
+      <b className="text-uppercase">tiny</b>
+    </Badge>{" "}
+    <span className="fw-normal text-dark">Pups</span>
+  </>
+);
+
+export default Logo;
