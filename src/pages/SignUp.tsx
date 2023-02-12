398461db2ca4d40df6f48045b90c0d0a85f445d4
John_Lamy <johnclamy@gmail.com>
2023-02-12 10:08:20 +0000

    add the sign up form to SignUp.tsx page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,21 @@
+import PageWrapper from "../components/PageWrapper";
+import FormWrapper from "../components/FormWrapper";
+import SignUpForm from "../components/SignUpForm";
+import { Link } from "react-router-dom";
+import Path from "../routes/Path";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <PageWrapper>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "6rem" }}
+      >
+        <FormWrapper>
+          <SignUpForm />
+        </FormWrapper>
+      </div>
+    </PageWrapper>
+  );
 };
 
 export default SignUp;
