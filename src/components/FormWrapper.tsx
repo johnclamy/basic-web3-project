ca464847d5af75a867d5b5f3d9bd16ca5ff6a209
John_Lamy <johnclamy@gmail.com>
2023-02-12 12:31:07 +0000

    add a title prop to theFormWrapper component

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -3,12 +3,13 @@ import Card from "react-bootstrap/Card";
 
 type FormWrapperProps = {
   children: ReactNode;
+  title: string;
 };
 
-const FormWrapper = ({ children }: FormWrapperProps) => (
+const FormWrapper = ({ children, title }: FormWrapperProps) => (
   <Card style={{ maxWidth: "400px" }}>
     <Card.Body>
-      <Card.Title className="text-center mb-4">Sign Up</Card.Title>
+      <Card.Title className="text-center mb-4">{title}</Card.Title>
       {children}
     </Card.Body>
   </Card>
