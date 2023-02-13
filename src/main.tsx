d18eec82ff5048dafe96cac6808d960686ede147
John_Lamy <johnclamy@gmail.com>
2023-02-13 23:59:01 +0000

    wrap app with AuthContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./services/auth/UserAuth";
 import App from "./app/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </BrowserRouter>
 );
