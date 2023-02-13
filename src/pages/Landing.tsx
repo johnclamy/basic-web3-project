132a53b0c66faf34fc62620128cb751cb54a07b6
John_Lamy <johnclamy@gmail.com>
2023-02-13 23:54:05 +0000

    create a landing page for unauthenticated users

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,34 @@
+import { Button, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import Path from "../routes/Path";
 import PageWrapper from "../components/PageWrapper";
 
 const Landing = () => {
-  return <PageWrapper>Content of the landing page goes here...</PageWrapper>;
+  const navigate = useNavigate();
+
+  return (
+    <PageWrapper>
+      <Card>
+        <Card.Header>Welcome to TinyPups pet management services</Card.Header>
+        <Card.Body>
+          <Card.Title className="display-6 text-primary mb-4">
+            Sign Up or Sign In
+          </Card.Title>
+          <Card.Text className="mb-5 fs-5 fst-italic  text-black-50">
+            This <b>pet management service</b> allows you to add pets to the pet
+            inventory and have your customers view them <em>instantly</em>.
+          </Card.Text>
+          <Button
+            className="text-uppercase"
+            variant="primary"
+            onClick={() => navigate(Path.SIGN_IN)}
+          >
+            sign in
+          </Button>
+        </Card.Body>
+      </Card>
+    </PageWrapper>
+  );
 };
 
 export default Landing;
