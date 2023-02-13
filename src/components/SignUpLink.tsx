d4ce6afbeb13a7d78c659cd3ca453b51f51ff9b6
John_Lamy <johnclamy@gmail.com>
2023-02-13 23:52:33 +0000

    setup a signUp link

--- src/components/SignUpLink.tsx
+++ src/components/SignUpLink.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 import Path from "../routes/Path";
 
 const SignUpLink = () => (
-  <p>
-    Don't have an account? <Link to={Path.SIGN_UP}>Sign Up.</Link>
+  <p className="mt-4 mb-2 text-center text-dark small">
+    Don't have an account?{" "}
+    <Link className="text-decoration-none" to={Path.SIGN_UP}>
+      Sign Up.
+    </Link>
   </p>
 );
 
