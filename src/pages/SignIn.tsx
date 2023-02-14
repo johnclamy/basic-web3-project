4598b36f8c0eba02ad73f390b2a228ce8a56b6c7
John_Lamy <johnclamy@gmail.com>
2023-02-14 08:29:24 +0000

    add sign in form to SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,21 @@
+import PageWrapper from "../components/PageWrapper";
+import FormWrapper from "../components/FormWrapper";
+import SignInForm from "../components/SignInForm";
+import ForgotPasswordLink from "../components/ForgotPasswordLink";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <PageWrapper>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "6rem" }}
+      >
+        <FormWrapper title="Sign In">
+          <SignInForm />
+        </FormWrapper>
+      </div>
+      <ForgotPasswordLink />
+    </PageWrapper>
+  );
 };
 
 export default SignIn;
