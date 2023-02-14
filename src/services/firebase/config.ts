6bf103f960c93c2332bc7755f4e8885a6ed21664
John_Lamy <johnclamy@gmail.com>
2023-02-14 08:26:11 +0000

    change from .env.local to .env

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -8,12 +8,12 @@ export type FirebaseConfig = {
 };
 
 const firebaseConfig: FirebaseConfig = {
-  apiKey: import.meta.env.local.VITE_API_KEY,
-  authDomain: import.meta.env.local.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.local.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.local.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.local.VITE_MESSAGEING_SENDER_ID,
-  appId: import.meta.env.local.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGEING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 export default firebaseConfig;
