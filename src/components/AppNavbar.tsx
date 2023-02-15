79721be94c053cc82abdb4df66316d42a496aa54
John_Lamy <johnclamy@gmail.com>
2023-02-15 07:52:43 +0000

    refactor AppNavbar.tsx for conditional nav links

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,69 +1,22 @@
-import { Link, useNavigate } from "react-router-dom";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Container, Navbar } from "react-bootstrap";
 import Logo from "../components/Logo";
 import Path from "../routes/Path";
 import UserAuth from "../services/auth/UserAuth";
+import SignedInNav from "./SignedInNav";
+import SignedOutNav from "./SignedOutNav";
 
 const AppNavbar = () => {
-  const navigate = useNavigate();
-  const { logout } = UserAuth();
-
-  const handleLogout = async () => {
-    try {
-      await logout();
-      navigate(Path.LANDING);
-      console.log("user is logged out");
-    } catch (err) {
-      if (err instanceof Error) {
-        console.error(err.name);
-      }
-    }
-  };
+  const { user } = UserAuth();
 
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand as={Link} to={Path.LANDING}>
+        <Navbar.Brand as={Link} to={user ? Path.HOME : Path.LANDING}>
           <Logo />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link as={Link} to={Path.HOME}>
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to={Path.ADD_PET}>
-              Add Pet
-            </Nav.Link>
-          </Nav>
-          <div className="d-flex gap-lg-3">
-            <Button
-              className="btn-sm"
-              variant="outline-primary"
-              onClick={() => navigate(Path.SIGN_UP)}
-            >
-              Sign Up
-            </Button>
-            <Button
-              className="btn-sm"
-              variant="outline-secondary"
-              onClick={() => navigate(Path.SIGN_IN)}
-            >
-              Sign In
-            </Button>
-            <Button
-              className="btn-sm"
-              variant="outline-secondary"
-              onClick={handleLogout}
-            >
-              Logout
-            </Button>
-          </div>
-        </Navbar.Collapse>
+        {user ? <SignedInNav user={user} /> : <SignedOutNav />}
       </Container>
     </Navbar>
   );
