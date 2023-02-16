1652f1654235f4138cfc30af73b0eb54aed0f5cf
John_Lamy <johnclamy@gmail.com>
2023-02-16 15:18:44 +0000

    set the pet collection in a PetList component

---
+++ src/app/pets/PetList.tsx
@@ -0,0 +1,14 @@
+import { Col, Row } from "react-bootstrap";
+import { useState } from "react";
+import data from "../../services/db/temp-data";
+import Pet from "./Pet";
+import PetCard from "./PetCard";
+
+const PetList = () => {
+  const [pets, setPets] = useState<Pet[]>(data as Pet[]);
+  const petList = pets.map((pet) => <PetCard key={pet.breed} pet={pet} />);
+
+  return <div>{petList}</div>;
+};
+
+export default PetList;
