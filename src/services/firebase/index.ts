0e5df9e7bee5abc5b8144e2ef959a29639c29358
John_Lamy <johnclamy@gmail.com>
2023-02-18 08:34:04 +0000

    add connection to Firebase Firestore

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import firebaseConfig from "./config";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 export default app;
-export { auth };
+export { auth, db };
