09afd4c70675d16bd4a5ca63687fc0d2b28c72d7
John_Lamy <johnclamy@gmail.com>
2023-02-18 15:04:37 +0000

    initialize Firebase Storage in firebase/index.ts

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -1,11 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import firebaseConfig from "./config";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 export default app;
-export { auth, db };
+export { auth, db, storage };
