8b695fa7256a7d29945be884d6a01a7f192c4bbb
John_Lamy <johnclamy@gmail.com>
2023-02-18 08:42:11 +0000

    load pet data from the api to state

--- src/app/pets/PetList.tsx
+++ src/app/pets/PetList.tsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
-import data from "../../services/db/temp-data";
+import { useEffect, useState } from "react";
+// import data from "../../services/db/temp-data";
 import Pet from "./Pet";
 import PetCard from "./PetCard";
+import { fetchPets } from "../../services/db";
+
+let data: Pet[] | [] = [];
 
 const PetList = () => {
-  const [pets, setPets] = useState<Pet[]>(data as Pet[]);
+  const [pets, setPets] = useState<Pet[]>(data);
   const petList = pets.map((pet) => <PetCard key={pet.breed} pet={pet} />);
+
+  useEffect(() => {
+    fetchPets(setPets);
+  }, []);
+
+  console.log(pets);
 
   return <div>{petList}</div>;
 };
