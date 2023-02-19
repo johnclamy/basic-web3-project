de42240d26e1e7949347e8d34930df320fc27a5d
John_Lamy <johnclamy@gmail.com>
2023-02-19 08:28:35 +0000

    remove local data and fetch from Firebase

--- src/app/pets/PetList.tsx
+++ src/app/pets/PetList.tsx
@@ -1,20 +1,35 @@
 import { useEffect, useState } from "react";
-// import data from "../../services/db/temp-data";
+import { listAll, getDownloadURL } from "firebase/storage";
 import Pet from "./Pet";
 import PetCard from "./PetCard";
 import { fetchPets } from "../../services/db";
+import { petImagesRef } from "../../services/storage";
+
+type PetImages = {
+  petImages: string[] | [];
+};
 
 let data: Pet[] | [] = [];
 
 const PetList = () => {
+  const [petImages, setPetImages] = useState<PetImages>([]);
   const [pets, setPets] = useState<Pet[]>(data);
-  const petList = pets.map((pet) => <PetCard key={pet.breed} pet={pet} />);
+  const petList = pets.map((pet) => (
+    <PetCard key={pet.breed} pet={pet} img={petImages} />
+  ));
 
   useEffect(() => {
     fetchPets(setPets);
+    listAll(petImagesRef).then((res) => {
+      res.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setPetImages((prev) => [...prev, url]);
+        });
+      });
+    });
   }, []);
 
-  console.log(pets);
+  console.log(petImages);
 
   return <div>{petList}</div>;
 };
