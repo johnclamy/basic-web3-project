05cc1afdcb2122b0e0096a4becd77dd77815635b
John_Lamy <johnclamy@gmail.com>
2023-02-20 20:20:10 +0000

    add form to page AddPet.tsx

--- src/pages/AddPet.tsx
+++ src/pages/AddPet.tsx
@@ -1,5 +1,19 @@
+import PageWrapper from "../components/PageWrapper";
+import FormWrapper from "../components/FormWrapper";
+import PetForm from "../app/pets/PetForm";
+
 const AddPet = () => {
-  return <div>AddPet</div>;
+  return (
+    <PageWrapper>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "6rem" }}
+      >
+        <FormWrapper title="Add a Pet">
+          <PetForm />
+        </FormWrapper>
+      </div>
+    </PageWrapper>
+  );
 };
 
 export default AddPet;
