55caa9736dbde8350ec9ab2ea7e1445ae2a39603
John_Lamy <johnclamy@gmail.com>
2023-02-21 11:25:32 +0000

    export the pet collection

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -2,10 +2,12 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import Pet from "../../app/pets/Pet";
 
+export const PET_COLLECTION = collection(db, "pets");
+
 export const fetchPets = async (
   setState: React.Dispatch<React.SetStateAction<Pet[]>>
 ) => {
-  const petsSnap = await getDocs(collection(db, "pets"));
+  const petsSnap = await getDocs(PET_COLLECTION);
   const petsData = petsSnap.docs.map((doc) => {
     return {
       ...doc.data(),
