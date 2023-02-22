8d8859ff660223986c50c81d2261d065a1e73f01
John_Lamy <johnclamy@gmail.com>
2023-02-22 09:15:33 +0000

    replace PetDetailsPage.tsx with PetUpdatePage.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home";
 import LandingPage from "../pages/Landing";
 import AddPetPage from "../pages/AddPet";
-import PetDetailsPage from "../pages/PetDetails";
+import PetUpdatePage from "../pages/PetUpdate";
 import NotFoundPage from "../pages/NotFound";
 import SignUpPage from "../pages/SignUp";
 import SignInPage from "../pages/SignIn";
@@ -19,7 +19,7 @@ const AppRoutes = () => {
       <Route element={<ProtectedRoutes />}>
         <Route path={Path.HOME} element={<HomePage />} />
         <Route path={Path.ADD_PET} element={<AddPetPage />} />
-        <Route path={Path.PET_DETAILS} element={<PetDetailsPage />} />
+        <Route path={Path.UPDATE_PET} element={<PetUpdatePage />} />
         <Route path={Path.NOT_FOUND} element={<NotFoundPage />} />
         <Route path={Path.PASSWORD_FORGET} element={<PasswordForgetPage />} />
       </Route>
