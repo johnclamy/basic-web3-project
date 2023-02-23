011fc3a781c074336262fa2ad75cd1e38f6dcf4b
John_Lamy <johnclamy@gmail.com>
2023-02-23 00:02:23 +0000

    setup a constant PET_COLLECTION_TITLE

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -2,7 +2,8 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import Pet from "../../app/pets/Pet";
 
-export const PET_COLLECTION = collection(db, "pets");
+export const PET_COLLECTION_TITLE = "pets";
+export const PET_COLLECTION = collection(db, PET_COLLECTION_TITLE);
 
 export const fetchPets = async (
   setState: React.Dispatch<React.SetStateAction<Pet[]>>
